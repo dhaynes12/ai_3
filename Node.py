b7130ea5a3e3b6cede68d76aca21693d66f0853b
Devan Haynes <dhaynes12@alaska.edu>
2019-10-10 11:39:29 -0800

    Added node generation

--- Node.py
+++ Node.py
@@ -1,37 +1,48 @@
-import board
+import board.py
 
 class Node(object):
     
-    def __init__(self, state, depth, depthLim, alpha, beta):
+    def __init__(self, state, depth, depthLim):
         self.state = state          # board state of the node
         self.depth = depth          # current depth of node
         self.depthLim = depthLim    # depth of how far the tree can evaluate
-        self.alpha = alpha          # alpha and beta are values for pruning the tree
-        self.beta = beta            
+        self.alpha = 0              # alpha and beta are values for pruning the tree
+        self.beta = 0            
         self.nextMoves = []         # list of next possible moves
         self.weight = 0             # How good of a move is current node
 
         # if depth is at limit call weight function
         if depth == depthLim:
-            pass
+            setWeight()
         else:       # else generate list of next possible moves
             genNextMoves()
 
 
     def genNextMoves(self):
-        nextStates = []
-        # Need to fix for whos turn it is i.e. 0 is CPU and 1 is human
-        for i in range(0,6):
-            if self.state.spaces[i] == 0:
-                continue
-            else:
-                # need to add optional state pass to board
-                tempState = board.getStartBoard()
-                self.nextMoves.append(Node())
+        if self.depth % 2 == 0:
+            for i in range(7,13):
+                if self.state.spaces[i] == 0:
+                    continue
+                else:
+                    # need to add optional state pass to board
+                    tempState = self.state[:]
+                    move(tempState, 1, i)
+                    tempNode = Node(tempState, self.depth+1, self.depthLim)
+                    self.nextMoves.append(tempNode)
+        else:
+            for i in range(0,6):
+                if self.state.spaces[i] == 0:
+                    continue
+                else:
+                    # need to add optional state pass to board
+                    tempState = self.state[:]
+                    move(tempState, 0, i)
+                    tempNode = Node(tempState, self.depth+1, self.depthLim)
+                    self.nextMoves.append(tempNode)
 
     # Generate how good a move is if node is a leaf node
     def setWeight(self):
-        pass
+        self.weight = self.state[13] - self.state[6]
 
 
 
